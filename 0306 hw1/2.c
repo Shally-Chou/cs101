f649e5fa54f1b51a58e1315842fd7c75a4882249
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2024-03-07 00:39:30 +0800

    Update 2.c

--- 0306 hw1/2.c
+++ 0306 hw1/2.c
@@ -1,10 +1,6 @@
 /******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
+input : A2a1B2b1C4
+output : AAaBBbCCCC
 *******************************************************************************/
 #include <stdio.h>
 #include <string.h>
