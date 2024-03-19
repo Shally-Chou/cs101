9f9a4e05df82990858d59b65767e023b96d654e8
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2024-03-19 23:15:20 +0800

    Add files via upload

---
+++ hw2 wrong/3 wrong.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    FILE* fp_in , *fp_out ;
+    fp_in = fopen("main.c" , "r") ;
+    char line[1000] ;
+    int found_main = 0 ;
+    int line_number = 1 ;
+    
+    fp_out = fopen("main2.txt", "w+");
+    for (;;) {
+        if (fgets(line, sizeof(line), fp_in) == NULL)
+            break;
+        if (!found_main && strstr(line, "int main()")) {
+            sprintf(line, "%d", line_number);
+            fputs(line,fp_out) ;
+            break;
+        }
+        line_number ++ ;
+        
+    }
+    
+    fclose(fp_in) ;
+    fclose(fp_out) ;
+
+    return 0;
+}
