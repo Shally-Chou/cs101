1c5d45204ec3166164ba47b409e8c88879420c97
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2024-03-20 00:07:14 +0800

    Update 2-1 wrong.c

--- hw2 wrong/2-1 wrong.c
+++ hw2 wrong/2-1 wrong.c
@@ -5,7 +5,7 @@ int main()
 {
     
     FILE* fp_in , *fp_out ;
-    fp_in = fopen("main.c" , "r") ;
+    fp_in = fopen("main.c" , "r") ;  //改成main2.c就過了
     int a_w[3] = {0, 1, 2};
     int a_r[3];
     char line[1000];
