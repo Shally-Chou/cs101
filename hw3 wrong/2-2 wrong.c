3e7fa912a0a68c5ddf518f615326f9a625b61aed
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2024-03-29 23:28:58 +0800

    Update 2-2 wrong.c

--- hw3 wrong/2-2 wrong.c
+++ hw3 wrong/2-2 wrong.c
@@ -14,13 +14,13 @@ int main()
 {
 
     char str[100], oldChar = 'a', newChar = 'A';
-    printf("Enter any string: %s", str);
+    printf("Enter any string: %s", str);  //刪掉 操
     scanf("%s", str);
-    printf("\nString before replacing:\n%s\n\n", str);
+    printf("\nString before replacing:\n%s\n\n", str); //刪掉 你圖片input明明就有這些
 
     replace_all(str, oldChar, newChar);
 
-    printf("String after replacing a with A:\n%s", str);
+    printf("String after replacing a with A:\n%s", str);  //內文刪掉留output 然後就會對 結果全部delet才算對 哭死欸真的
 
     return 0;
 }
