a4f671572c95ca97e197b9aab2b9e9c0e80f4381
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2024-03-29 23:07:56 +0800

    Add files via upload

---
+++ hw3 wrong/2-1 wrong.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+void replace_all(char *str, char oldChar, char newChar) {
+    int lenth = strlen(str) ;
+    for(int i = 0 ; i < lenth ; i++){
+        if(str[i] == 'a'){
+            oldChar = newChar ;
+            str[i] = oldChar ;
+        }
+    }
+}
+
+int main()
+{
+
+    char str[100], oldChar = 'a', newChar = 'A';
+    printf("Enter any string: %s", str);
+    scanf("%s", str);
+    printf("\nString before replacing:\n%s\n\n", str);
+
+    replace_all(str, oldChar, newChar);
+
+    printf("String after replacing a with A:\n%s", str);
+
+    return 0;
+
+}
