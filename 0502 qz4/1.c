7e0124dc9ea6b3f30bc83d03293ba92e4b85f435
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2024-06-05 19:00:10 +0800

    Create 1.c

---
+++ 0502 qz4/1.c
@@ -0,0 +1,36 @@
+/******************************************************************************
+使用 calloc 和 free 複製一個 string
+輸出經過clone_string() 複製後的字元
+output : The cloned string: (這裡就scanf的值)
+*******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *clone_string(char *str) {
+    char *cloned_str = (char *)calloc(strlen(str) + 1, sizeof(char));
+    if (cloned_str != NULL) {
+        strcpy(cloned_str, str);
+    }
+    return cloned_str;
+}
+
+void free_string(char *str) {
+    free(str);
+}
+
+int main() {
+    char str[100];
+    scanf("%99s", str);
+    
+    char *pstr = clone_string(str);
+    
+    if (pstr != NULL) {
+        printf("The cloned string: %s\n", pstr);
+        free_string(pstr);
+    } else {
+        printf("Memory allocation failed.\n");
+    }
+    
+    return 0;
+}
