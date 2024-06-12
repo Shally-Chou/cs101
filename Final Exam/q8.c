3a2818206dfe725892c65586124cc4cfaece53cc
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2024-06-12 16:42:31 +0800

    Update Final Exam

---
+++ Final Exam/q8.c
@@ -0,0 +1,16 @@
+include <stdio.h>
+
+int rec_Fibonacci(int n){
+    if(n<1){
+        return n;
+    }
+    
+    return n+rec_Fibonacci(n-1);
+}
+
+int main(){
+    int n = 10;
+    int sum = rec_Fibonacci(n);
+    printf("Fibonacci(%d) = %d", n , sum);
+    return 0;
+}
