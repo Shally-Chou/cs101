00c146b6c7adf7ce6bf62527809c974c15985ee0
Shally-Chou <144075091+Shally-Chou@users.noreply.github.com>
2024-06-12 16:22:51 +0800

    Update

---
+++ Final Exam/ q6.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    FILE* fp_in;
+    char line[100];
+    int num = 1;
+    int found = 0;
+    
+    fp_in = fopen("main.c","r");
+    for(;;){
+        if(fgets(line,sizeof(line),fp_in)==NULL){
+            return 0;
+        }
+        if(!found && strstr(line,"int main(){")){
+            strcpy(line,"int main(){");
+            printf("%d %s",num,line);
+            return 0;
+        }
+        num++;
+    }
+    
+    fclose(fp_in);
+    return 0;
+}
